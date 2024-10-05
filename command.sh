17c99d35b1926f7e0409a719d8d091012783bed7
nhanse1311 <49554489+nhanse1311@users.noreply.github.com>
2024-10-05 14:18:46 +0700

    update python version 3.10

--- command.sh
+++ command.sh
@@ -8,7 +8,7 @@ RESOURCE_GROUP="Azuredevops"
 #az group create --name $RESOURCE_GROUP --location "East US"
 
 # Create the web app
-az webapp up -n $APP_NAME --resource-group $RESOURCE_GROUP --runtime "PYTHON:3.8" --sku B1
+az webapp up -n $APP_NAME --resource-group $RESOURCE_GROUP --runtime "PYTHON:3.10" --sku B1
 
 # Show the deployed URL
 echo "Your app is deployed at: https://$APP_NAME.azurewebsites.net"
